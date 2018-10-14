29c8d1ea1315515ef758e70201e406ad9d8c7476
Carlos Cadena <cjcs.cadenasarasty8@gmail.com>
2018-10-13 21:41:48 -0500

    avance en la lista de producto del index

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
 <?php include 'Header.html';?>
-<h1>Bienvenido al sistema de venta</h1>
+        
+        <div class="container">
+            <h1>Bienvenido al sistema de venta</h1>
+            <div class="row">
+                <div class="col-xl">
+                    <div class="card" style="width:18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">hola</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                            <p class="card-text">
+
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
 
 <?php include("Footer.html"); ?>
